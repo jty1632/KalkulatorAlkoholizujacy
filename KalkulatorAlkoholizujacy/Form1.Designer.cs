b1ba6d55dc891002cdfc53e271d18137b6572c27
piotr <piotrsosnwoski1632@gmail.com>
2024-10-18 21:52:32 +0200

    poprawki wizualne

--- KalkulatorAlkoholizujacy/Form1.Designer.cs
+++ KalkulatorAlkoholizujacy/Form1.Designer.cs
@@ -71,9 +71,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.Location = new System.Drawing.Point(501, 111);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(0, 25);
             this.label1.TabIndex = 4;
             // 
             // button1
