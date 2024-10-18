3e12372c7b4733c95f4d03bb453e417125bd1431
piotr <piotrsosnwoski1632@gmail.com>
2024-10-18 21:09:16 +0200

    podstawowy layout

--- KalkulatorAlkoholizujacy/Form1.Designer.cs
+++ KalkulatorAlkoholizujacy/Form1.Designer.cs
@@ -31,54 +31,46 @@
             this.comboBoxDish = new System.Windows.Forms.ComboBox();
             this.comboBoxDrink = new System.Windows.Forms.ComboBox();
             this.textBoxPieces = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBoxDish
             // 
-            this.comboBoxDish.Dock = System.Windows.Forms.DockStyle.Top;
             this.comboBoxDish.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
             this.comboBoxDish.FormattingEnabled = true;
-            this.comboBoxDish.Location = new System.Drawing.Point(0, 33);
+            this.comboBoxDish.Location = new System.Drawing.Point(130, 16);
             this.comboBoxDish.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
             this.comboBoxDish.Name = "comboBoxDish";
-            this.comboBoxDish.Size = new System.Drawing.Size(260, 33);
+            this.comboBoxDish.Size = new System.Drawing.Size(200, 33);
             this.comboBoxDish.TabIndex = 0;
             // 
             // comboBoxDrink
             // 
-            this.comboBoxDrink.Dock = System.Windows.Forms.DockStyle.Top;
-            this.comboBoxDrink.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.comboBoxDrink.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
             this.comboBoxDrink.FormattingEnabled = true;
-            this.comboBoxDrink.Location = new System.Drawing.Point(0, 0);
+            this.comboBoxDrink.Location = new System.Drawing.Point(95, 61);
             this.comboBoxDrink.Name = "comboBoxDrink";
-            this.comboBoxDrink.Size = new System.Drawing.Size(260, 33);
+            this.comboBoxDrink.Size = new System.Drawing.Size(235, 33);
             this.comboBoxDrink.TabIndex = 1;
             // 
             // textBoxPieces
             // 
-            this.textBoxPieces.Location = new System.Drawing.Point(509, 181);
+            this.textBoxPieces.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBoxPieces.Location = new System.Drawing.Point(86, 109);
             this.textBoxPieces.Name = "textBoxPieces";
-            this.textBoxPieces.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPieces.Size = new System.Drawing.Size(244, 32);
             this.textBoxPieces.TabIndex = 2;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.comboBoxDish);
-            this.panel1.Controls.Add(this.comboBoxDrink);
-            this.panel1.Location = new System.Drawing.Point(107, 99);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(260, 137);
-            this.panel1.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(622, 310);
+            this.label1.Location = new System.Drawing.Point(487, 105);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 13);
             this.label1.TabIndex = 4;
@@ -86,26 +78,71 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(494, 360);
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei Light", 13F);
+            this.button1.Location = new System.Drawing.Point(12, 169);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(336, 43);
             this.button1.TabIndex = 5;
-            this.button1.Text = "buttonResult";
+            this.button1.Text = "O B L I C Z";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(6, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "NACZYNIE:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxPieces);
+            this.groupBox1.Controls.Add(this.comboBoxDrink);
+            this.groupBox1.Controls.Add(this.comboBoxDish);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(336, 151);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(6, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "NAPÓJ:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(6, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 28);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ILOŚĆ:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(691, 234);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.textBoxPieces);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
             this.Text = "Kalkulator Alkoholizujący";
-            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,9 +153,12 @@
         private System.Windows.Forms.ComboBox comboBoxDish;
         private System.Windows.Forms.ComboBox comboBoxDrink;
         private System.Windows.Forms.TextBox textBoxPieces;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
