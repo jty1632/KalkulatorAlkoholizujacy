fe09acb538ea1708e9b0e4dfd19e9f15f0ad162f
piotr <piotrsosnwoski1632@gmail.com>
2024-10-18 21:11:49 +0200

    wyczyszczenie labelResult

--- KalkulatorAlkoholizujacy/Form1.Designer.cs
+++ KalkulatorAlkoholizujacy/Form1.Designer.cs
@@ -70,11 +70,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(487, 105);
+            this.label1.Location = new System.Drawing.Point(501, 111);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "labelResult";
             // 
             // button1
             // 
